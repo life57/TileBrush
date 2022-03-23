275ae8f5f83ac4af3bb4c92ef7c1e1f5820b1abc
life57 <42088083+life57@users.noreply.github.com>
2022-03-23 14:10:01 +0200

    Add files via upload

---
+++ MainViewModel.cs
@@ -0,0 +1,75 @@
+﻿using GalaSoft.MvvmLight;
+using GalaSoft.MvvmLight.Command;
+using System;
+using System.Windows;
+
+namespace TiledImage
+{
+    public class MainViewModel : ViewModelBase
+    {
+        private double imageWidth;
+        private double imageHeight;
+        private double repeatX;
+        private double rectangleHeight;
+        private double rectangleWidth;
+
+        public MainViewModel()
+        {
+            ImageWidth = 259;
+            ImageHeight = 194;
+            RepeatX = 1.0;
+            RectangleHeight = ImageHeight;
+            RectangleWidth = imageWidth;
+
+            OkCommand = new RelayCommand(OnOk);
+        }
+
+        private void OnOk()
+        {
+            RectangleHeight = RepeatX * ImageHeight;
+            RaisePropertyChanged("RepeatX");
+        }
+
+        public double ImageWidth
+        {
+            get => imageWidth;
+            set
+            {
+                Set(ref imageWidth, value);
+            }
+        }
+        public double ImageHeight
+        {
+            get => imageHeight;
+            set
+            {
+                Set(ref imageHeight, value);
+            }
+        }
+        public double RepeatX
+        {
+            get => repeatX;
+            set
+            {
+                Set(ref repeatX, value);
+            }
+        }
+        public double RectangleHeight
+        {
+            get => rectangleHeight;
+            set
+            {
+                Set(ref rectangleHeight, value);
+            }
+        }
+        public double RectangleWidth
+        {
+            get => rectangleWidth;
+            set
+            {
+                Set(ref rectangleWidth, value);
+            }
+        }
+        public RelayCommand OkCommand { get; set; }
+    }
+}
---
+++ ViewPortHeightConverter.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Globalization;
+using System.Windows;
+using System.Windows.Data;
+
+namespace TiledImage
+{
+    public class ViewPortHeightConverter : IValueConverter
+    {
+        public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            double h = 1 / (double)value;
+            return new Rect(0, 1, 1, h);
+        }
+
+        public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
